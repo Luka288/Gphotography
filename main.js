35c16cd1a7f79a25f97d82005a007e49b7831d9e
Luka288 <75569044+Luka288@users.noreply.github.com>
2024-05-08 21:59:50 +0400

    done paralax

--- main.js
+++ main.js
@@ -4,11 +4,29 @@ window.onmousedown = (e) => {
   track.dataset.mouseDownAt = e.clientX;
 };
 
+window.onmouseup = () => {
+  track.dataset.mouseDownAt = "0";
+  track.dataset.prevPercentage = track.dataset.percentage;
+};
+
 window.onmousemove = (e) => {
-    if (track.dataset.mouseDownAt === 0) return;
-  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-    maxDelta = window.innerWidth / 2;
+  if (track.dataset.mouseDownAt === "0") return;
+
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
+  const maxDelta = window.innerWidth / 2;
 
   const percentage = (mouseDelta / maxDelta) * -100,
-  nextPersentage =parseFloat(track.dataset.prevpercentage) + percentage
+    nextPercentageUnconstrained =
+      parseFloat(track.dataset.prevPercentage) + percentage,
+    nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+
+  track.dataset.percentage = nextPercentage;
+
+  track.style.transform = `translate(${nextPercentage}%, -50%)`;
+
+  const images = track.getElementsByClassName("image");
+  for (const image of images) {
+    const offsetX = nextPercentage + 100;
+    image.style.objectPosition = `${offsetX}% 50%`;
+  }
 };
